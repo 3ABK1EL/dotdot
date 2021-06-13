90542332935ad7c861a190237f10f20918e20c69
3ABK1EL <80121838+3ABK1EL@users.noreply.github.com>
2021-06-13 16:55:45 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
   name='dotdot',
   description='',
-  version='1.0.0',
+  version='1.1.0',
   author='3ABK1EL',
   author_email='None@None.net',
   packages=['dotdot']
